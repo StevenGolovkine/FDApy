873a4cac6f3cb56a964086f4a285f10210ebff7e
StevenGolovkine <steven_golovkine@icloud.com>
2018-10-29 10:14:04 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='FDApy',
+	version='0.1',
+	description='Python package for Functional Data Analysis',
+	url='',
+	author='Steven Golovkine',
+	author_email='steven_golovkine@icloud.com',
+	license='MIT',
+	packages=['FDApy'],
+	zip_safe=False)
