e90cf0546fd29c74dae5b0ff6ada780e71250aac
StevenGolovkine <steven_golovkine@icloud.com>
2018-12-05 16:47:42 +0100

    Add Wiener process basis

--- FDApy/basis.py
+++ FDApy/basis.py
@@ -9,13 +9,13 @@ import FDApy
 def basis_legendre(degrees=3, argvals=None):
 	"""Define Legendre basis of function.
 	
-	Build a basis of functions using Legendre polynomials with degree `degree^ on the interval `argvals`.
+	Build a basis of functions using Legendre polynomials with degree `degree` on the interval `argvals`.
 
 	Parameters
 	----------
 	degree : int, default = 3
 		Maximum degree of the Legendre polynomials. 
-	argvals : tuple, default = None
+	argvals : tuple or numpy.ndarray, default = None
 		The values on which evaluated the Legendre polynomials. If `None`, the polynomials are evaluated on the interval [-1, 1].
 
 	Return
@@ -47,3 +47,40 @@ def basis_legendre(degrees=3, argvals=None):
 	obj = FDApy.univariate_functional.UnivariateFunctionalData(
 			tuple(argvals), values)
 	return obj
+
+def basis_wiener(degrees=3, argvals=None):
+	"""Define Wiener basis of function.
+
+	Build a basis of functions of the Wiener process.
+
+	Parameters
+	----------
+	degree : int, default = 3
+		Number of functions to compute.
+	argvals : tuple or numpy.ndarray, default = None
+		 The values on which evaluated the Wiener basis functions. If `None`, the functions are evaluated on the interval [0, 1].
+
+	Return
+	------
+	obj : FDApy.univariate_functional.UnivariateFunctionalData
+		A UnivariateFunctionalData object containing `degrees` Wiener basis functions evaluated on `argvals`.
+
+	"""
+	if argvals is None:
+		argvals = np.arange(0, 1, 0.05)
+
+	if isinstance(argvals, list):
+		raise ValueError('argvals has to be a tuple or a numpy array!')
+
+	if isinstance(argvals, tuple):
+		argvals = np.array(argvals)
+
+	values = np.empty((degrees, len(argvals)))
+
+	for degree in range(degrees):
+		wiener = np.sqrt(2) * np.sin( (degree - 0.5) * np.pi * argvals)
+		values[degree, :] = wiener
+
+	obj = FDApy.univariate_functional.UnivariateFunctionalData(
+			tuple(argvals), values)
+	return obj 
