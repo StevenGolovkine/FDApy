f0c0327d6d90330275ea15f04713a694a488bf75
StevenGolovkine <steven_golovkine@icloud.com>
2020-03-31 09:07:55 +0200

    Add bandwidth

---
+++ FDApy/bandwidth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3.7
+# -*-coding:utf8 -*
+
+import numpy as np
+import scipy
+
+import FDApy
+
+##############################################################################
+# Class Bandwidth
+
+class Bandwidth(object):
+    """An object to define a bandwidth for the smoothing.
+
+    Parameters
+    ----------
+    t0 : list of float
+        A list of sampling points
+    k0 : list of int
+        A list of neighborhood
+    H : list of float
+        A list of Hurst parameters
+    L : list of float
+        A list of constant
+
+    Attributes
+    ----------
+    b : list of float
+        Estimation of the bandwidth for each t0
+    """
+    def __init__(self):
+
+
+    def __repr__(self):
+        res = "Bandwidth:\n" +\
+            "\tSampling points: " + str(self.t0) + "\n" +\
+            "\tHurst estimates: " + str(self.H) + "\n" +\
+            "\tConstant estimates: " + str(self.L) + "\n" +\
+            "\tBandwidth estiates: " + str(self.b) + "."
+        return res
