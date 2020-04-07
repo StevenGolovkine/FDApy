35fd308f9c197b260897c8dcd3224ea5e9ca802b
StevenGolovkine <steven_golovkine@icloud.com>
2020-04-07 08:56:10 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='FDApy',
-      version='0.2',
+      version='0.3',
       description='Python package for Functional Data Analysis',
       long_description='',
       classifiers=[
