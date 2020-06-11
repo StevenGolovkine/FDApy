d0183f96fe9e9350abbdce88370ac48ea789339b
StevenGolovkine <steven_golovkine@icloud.com>
2020-06-11 17:17:58 +0200

    Test CI for deployment

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_readme():
 
 
 setup(name='FDApy',
-      version='0.3.6',
+      version='0.3.7',
       description='Python package for Functional Data Analysis',
       long_description=get_readme(),
       classifiers=[
