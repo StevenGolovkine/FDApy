6239a93b3f9428334bf445d96e3003da8c6ee64f
StevenGolovkine <steven_golovkine@icloud.com>
2020-06-12 11:45:05 +0200

    CI deployment

--- scripts/prepare_for_build.sh
+++ scripts/prepare_for_build.sh
@@ -3,9 +3,9 @@
 #sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 #sudo apt-get -q update
 #sudo apt-get -y install gcc-4.8
-sudo apt-get -y install libfreetype6-dev
-sudo apt-get -y install pkg-config
-sudo apt-get -y install libpng12-dev
-sudo apt-get -y install pkg-config
+apt-get -y install libfreetype6-dev
+apt-get -y install pkg-config
+apt-get -y install libpng12-dev
+apt-get -y install pkg-config
 
 pip install Cython
