bb79a61c2e53daf7750ad68bf528275cbdffa091
StevenGolovkine <steven_golovkine@icloud.com>
2020-09-23 17:56:27 +0200

    Modify color for color-blind people

--- FDApy/clustering/fcubt.py
+++ FDApy/clustering/fcubt.py
@@ -18,10 +18,11 @@ from ..preprocessing.dim_reduction.fpca import UFPCA, MFPCA
 from .optimalK.gap import Gap
 from .optimalK.bic import BIC
 
-from matplotlib import colors as mcolors
 from sklearn.mixture import GaussianMixture
 
-COLORS = [v for v in mcolors.BASE_COLORS.values()]
+COLORS = ['#377eb8', '#ff7f00', '#4daf4a',
+          '#f781bf', '#a65628', '#984ea3',
+          '#999999', '#e41a1c', '#dede00']
 
 
 ###############################################################################
