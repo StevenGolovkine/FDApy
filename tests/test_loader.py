638f7d3df40fec7b7a61638ace3a5e705064d85d
StevenGolovkine <steven_golovkine@icloud.com>
2023-01-28 19:51:33 +0000

    Add tests for loaders

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -7,16 +7,12 @@ Written with the help of ChatGPT.
 """
 import pandas as pd
 import numpy as np
-import os
 import unittest
 
 from FDApy.representation.functional_data import (
     DenseFunctionalData, IrregularFunctionalData
 )
 from FDApy.misc.loader import read_csv, _read_csv_dense, _read_csv_irregular
-
-
-DATA = os.path.join(os.path.dirname(__file__), 'data')
 
 
 class TestReadCsvDense(unittest.TestCase):
@@ -75,39 +71,62 @@ class TestReadCsvIrregular(unittest.TestCase):
         np.testing.assert_equal(obj.values, self.expected_values)
 
 
-class TestLoaderCSV(unittest.TestCase):
-    """Test class for the csv loader."""
+class TestReadCsv(unittest.TestCase):
+    def setUp(self):
+        # create a test csv file
+        self.dense_data = {
+            'x': [1, 2, 3, 4, 5],
+            'y': [2, 4, 6, 8, 10]
+        }
+        self.dense_df = pd.DataFrame(self.dense_data)
+        self.dense_df.to_csv('dense_test.csv', index=False)
 
-    def test_load_dense(self):
-        data = read_csv(os.path.join(DATA, 'dense.csv'), index_col=0)
-        self.assertEqual(data.n_dim, 1)
-        self.assertEqual(data.n_obs, 3)
-        self.assertTrue(
-            np.allclose(data.argvals['input_dim_0'], np.array([0, 1, 2, 3]))
+        self.irregular_data = {
+            'x': [1, 2, np.nan, 4],
+            'y': [2, np.nan, 6, 8]
+        }
+        self.irregular_df = pd.DataFrame(self.irregular_data)
+        self.irregular_df.to_csv('irregular_test.csv', index=False)
+
+    def test_read_csv_dense(self):
+        dense_obj = read_csv('dense_test.csv')
+        self.assertIsInstance(dense_obj, DenseFunctionalData)
+        np.testing.assert_array_equal(dense_obj.argvals['input_dim_0'], [0, 1])
+        np.testing.assert_array_equal(dense_obj.values, self.dense_df.values)
+
+    def test_read_csv_irregular(self):
+        irregular_obj = read_csv('irregular_test.csv')
+        self.assertIsInstance(irregular_obj, IrregularFunctionalData)
+        print(irregular_obj.argvals)
+        print(irregular_obj.values)
+        np.testing.assert_array_equal(
+            irregular_obj.argvals['input_dim_0'][0], np.array([0, 1])
         )
-        self.assertTrue(
-            np.allclose(
-                data.values,
-                np.array(
-                    [
-                        [4.6, 4.7, 4.2, 3.6],
-                        [4.6, 4.1, 4.1, 4.0],
-                        [4.7, 4.4, 4.4, 4.0]
-                    ]
-                )
-            )
+        np.testing.assert_array_equal(
+            irregular_obj.argvals['input_dim_0'][1], np.array([0])
+        )
+        np.testing.assert_array_equal(
+            irregular_obj.argvals['input_dim_0'][2], np.array([1])
+        )
+        np.testing.assert_array_equal(
+            irregular_obj.argvals['input_dim_0'][3], np.array([0, 1])
         )
 
-    def test_load_irregular(self):
-        data = read_csv(os.path.join(DATA, 'irregular.csv'), index_col=0)
-        self.assertEqual(data.n_dim, 1)
-        self.assertEqual(data.n_obs, 3)
-        self.assertTrue(
-            np.allclose(data.argvals['input_dim_0'][0], np.array([-2, 0, 3]))
+        np.testing.assert_array_equal(
+            irregular_obj.values[0], np.array([1, 2])
         )
-        self.assertTrue(
-            np.allclose(
-                data.values[0],
-                np.array([548, 605, 893])
-            )
+        np.testing.assert_array_equal(
+            irregular_obj.values[1], np.array([2])
         )
+        np.testing.assert_array_equal(
+            irregular_obj.values[2], np.array([6])
+        )
+        np.testing.assert_array_equal(
+            irregular_obj.values[3], np.array([4, 8])
+        )
+
+    def tearDown(self):
+        # delete test csv files
+        import os
+        os.remove('dense_test.csv')
+        os.remove('irregular_test.csv')
