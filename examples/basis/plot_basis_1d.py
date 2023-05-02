c201f7cb2035f6754b885d6414bb635d84115fd8
StevenGolovkine <steven_golovkine@icloud.com>
2023-05-02 16:20:47 +0200

    Examples basis 1D

--- examples/basis/plot_basis_1d.py
+++ examples/basis/plot_basis_1d.py
@@ -17,14 +17,33 @@ from FDApy.representation.basis import Basis
 from FDApy.visualization.plot import plot
 
 # Parameters
-name = 'fourier'
 n_functions = 5
 argvals = np.linspace(0, 1, 101)
 dimension = '1D'
 
 ###############################################################################
+# Fourier
 basis = Basis(
-    name=name, n_functions=n_functions, argvals=argvals, dimension=dimension
+    name='fourier', n_functions=n_functions,
+    argvals=argvals, dimension=dimension
+)
+
+_ = plot(basis)
+
+###############################################################################
+# B-splines
+basis = Basis(
+    name='bsplines', n_functions=n_functions,
+    argvals=argvals, dimension=dimension
+)
+
+_ = plot(basis)
+
+###############################################################################
+# Wiener
+basis = Basis(
+    name='wiener', n_functions=n_functions,
+    argvals=argvals, dimension=dimension
 )
 
 _ = plot(basis)
