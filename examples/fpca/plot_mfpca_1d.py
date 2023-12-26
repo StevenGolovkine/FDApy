933001340e5f89d6c70a2575e094fffeed498d68
StevenGolovkine <steven_golovkine@icloud.com>
2023-12-26 14:13:18 +0100

    Examples - MFPCA 1D

--- examples/fpca/plot_mfpca_1d.py
+++ examples/fpca/plot_mfpca_1d.py
@@ -31,16 +31,16 @@ colors = np.array([[0.5, 0, 0, 1]])
 name = ['bsplines', 'fourier']
 n_functions = 5
 
-# ###############################################################################
-# # We simulate :math:`N = 50` curves of a 2-dimensional process. The first
-# # component of the process is defined on the one-dimensional observation grid
-# # :math:`\{0, 0.01, 0.02, \cdots, 1\}`, based on the first :math:`K = 5`
-# # B-splines basis functions on :math:`[0, 1]` and the variance of the scores
-# # random variables equal to :math:`1`. The second component of the process is
-# # defined on the one-dimensional observation grid
-# # :math:`\{0, 0.01, 0.02, \cdots, 1\}`, based on the first :math:`K = 5`
-# # Fourier basis functions on :math:`[0, 1]` and the variance of the scores
-# # random variables equal to :math:`1`.
+###############################################################################
+# We simulate :math:`N = 50` curves of a 2-dimensional process. The first
+# component of the process is defined on the one-dimensional observation grid
+# :math:`\{0, 0.01, 0.02, \cdots, 1\}`, based on the first :math:`K = 5`
+# B-splines basis functions on :math:`[0, 1]` and the variance of the scores
+# random variables equal to :math:`1`. The second component of the process is
+# defined on the one-dimensional observation grid
+# :math:`\{0, 0.01, 0.02, \cdots, 1\}`, based on the first :math:`K = 5`
+# Fourier basis functions on :math:`[0, 1]` and the variance of the scores
+# random variables equal to :math:`1`.
 kl = KarhunenLoeve(
      basis_name=name, n_functions=n_functions, random_state=rng
 )
@@ -118,14 +118,8 @@ colors_innpro = np.array([[0.9, 0, 0.9, 1]])
 fig, axes = plt.subplots(nrows=5, ncols=2, figsize=(16,16))
 for idx_plot, idx in enumerate(np.random.choice(n_obs, 5)):
     for idx_data, (dd, dd_numint, dd_innpro) in enumerate(zip(kl.data.data, data_recons_numint.data, data_recons_innpro.data)):
-        axes[idx_plot, idx_data] = plot(dd[idx], ax=axes[idx_plot, idx_data])
-        axes[idx_plot, idx_data] = plot(dd_numint[idx], colors=colors_numint, ax=axes[idx_plot, idx_data])
-        axes[idx_plot, idx_data] = plot(dd_innpro[idx], colors=colors_innpro, ax=axes[idx_plot, idx_data])
-
-    #temp_ax = axes.flatten()[idx_plot]
-    #temp_ax = plot_multivariate(kl.data[idx], ax=temp_ax, label='True')
-    #plot_multivariate(data_recons_numint[idx], colors=colors_numint, ax=temp_ax, label='Reconstruction NumInt')
-    #plot_multivariate(data_recons_innpro[idx], colors=colors_innpro, ax=temp_ax, label='Reconstruction InnPro')
-    #temp_ax.legend()
+        axes[idx_plot, idx_data] = plot(dd[idx], ax=axes[idx_plot, idx_data], label='True')
+        axes[idx_plot, idx_data] = plot(dd_numint[idx], colors=colors_numint, ax=axes[idx_plot, idx_data], label='Reconstruction NumInt')
+        axes[idx_plot, idx_data] = plot(dd_innpro[idx], colors=colors_innpro, ax=axes[idx_plot, idx_data], label='Reconstruction InnPro')
+        axes[idx_plot, idx_data].legend()
 plt.show()
-
