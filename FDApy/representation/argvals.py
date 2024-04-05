Fix IrregularArgvals normalization (#12)

FDApy/representation/argvals.py
@@ -438,7 +438,10 @@ class IrregularArgvals(Argvals):
                 min_x, max_x = min_max[in_key]
                 if out_key not in stand_dict:
                     stand_dict[out_key] = DenseArgvals({})
-                stand_dict[out_key][in_key] = (value - min_x) / (max_x - min_x)
+                if min_x == max_x:
+                    stand_dict[out_key][in_key] = np.array([0])
+                else:
+                    stand_dict[out_key][in_key] = (value - min_x) / (max_x - min_x)
         return IrregularArgvals(stand_dict)
 
     def switch(self) -> Dict[str, Dict[int, npt.NDArray[np.float64]]]:

FDApy/representation/functional_data.py
@@ -2338,7 +2338,7 @@ class IrregularFunctionalData(FunctionalData):
 
             cov_count[mask] += 1
             cov_sum[mask] += cov
-        cov = np.where(cov_count == 0, np.nan, cov_sum / cov_count)
+        cov = np.divide(cov_sum, cov_count, where=(cov_count != 0))
         cov = cov.reshape(2 * n_points)
         raw_diag_cov = np.diag(cov).copy()
 